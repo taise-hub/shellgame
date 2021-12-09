d6032aa14cbc5c828bfe5fe724133e2037a432ef
井上大誠 <t.base.8901@icloud.com>
2021-12-09 19:14:58 +0900

    Add preparation process before satrting game

--- src/app/domain/service/battle.go
+++ src/app/domain/service/battle.go
@@ -1,19 +1,30 @@
 package service
 
 import (
+	"github.com/taise-hub/shellgame/src/app/domain/repository"
 	"github.com/taise-hub/shellgame/src/app/domain/model"
 )
 
 type BattleService interface {
+	Start(string) error
 	ParticipateIn(*model.Player, string)
+	Prepare(string)
 }
 
 type battleService struct {
-
+	socketRepo    repository.WebSocketRepository
+	containerSvc ContainerService
 }
 
-func NewBattleService() BattleService {
-	return &battleService{}
+func NewBattleService(socketRepo repository.WebSocketRepository, containerSvc ContainerService) BattleService {
+	return &battleService{
+		socketRepo: socketRepo,
+		containerSvc: containerSvc,
+	}
+}
+
+func (svc *battleService) Start(name string) error {
+	return svc.containerSvc.Start(name)
 }
 
 func (svc *battleService) ParticipateIn(player *model.Player, roomName string) {
@@ -28,3 +39,36 @@ func (svc *battleService) createRoom(name string) *model.Room {
 	}
 	return supervisor.CreateRoom(name)
 }
+
+func (svc *battleService) Prepare(name string) {
+	supervisor := model.GetSupervisor()
+	if supervisor.HasRoom(name) {
+		return
+	}
+	room := supervisor.GetRoom(name)
+	svc.prepare(room)
+}
+
+//多分これだけで大丈夫よね？
+func (svc *battleService) prepare(room *model.Room) {
+	// go room.Ticker()
+	// go room.CmdHub()
+	// go room.ScoreHub()
+}
+
+//FIXME: name is not appropriate.
+func (svc *battleService) reciever(player *model.Player) error {
+	var recv string
+	for {
+		err := svc.socketRepo.Read(player.Conn, recv)
+		if err != nil {
+			return err
+		}
+		switch recv {
+		// case "cmd":
+		// 	cmd := revc
+		// 	result, err := 
+		}
+	}
+}
+
