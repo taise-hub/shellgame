7ee440f202c0cefe81769ee0603718502bf468e7
井上大誠 <t.base.8901@icloud.com>
2021-12-09 16:22:00 +0900

    Add a model

---
+++ src/app/domain/model/supervisor.go
@@ -0,0 +1,52 @@
+package model
+
+var (
+	// preSupervisor *Supervisor = &Supervisor{rooms: make(map[string]*Room)}
+	supervisor *Supervisor = &Supervisor{rooms: make(map[string]*Room)}
+)
+
+// A supervisor manage battle room.
+// A supervisor is singleton.
+type Supervisor struct {
+	rooms map[string]*Room
+}
+// type Supervisor struct {
+// 	rooms []Hoge
+// }
+
+// type TaggedRoom struct {
+// 	ID	 string
+// 	room *Room
+// }
+
+func GetSupervisor() *Supervisor {
+	return supervisor
+}
+
+// func GetPreSupervisor() *Supervisor {
+// 	return preSupervisor
+// }
+
+func (spv *Supervisor) HasRoom(name string) bool {
+	 _, exist := spv.rooms[name]
+	 return exist
+}
+
+func (spv *Supervisor) GetRooms() map[string]*Room {
+	return spv.rooms
+}
+
+func (spv *Supervisor) GetRoom(name string) *Room {
+	return spv.rooms[name]
+}
+
+func (spv *Supervisor) SetRoom(name string, room *Room) {
+	spv.rooms[name] = room
+	return
+}
+
+func (spv *Supervisor) CreateRoom(name string) *Room {
+	room := NewRoom(name)
+	spv.SetRoom(room.Name, room)
+	return room
+}
