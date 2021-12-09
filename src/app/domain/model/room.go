741b1cdd5036a29181816559049cb46e09d9c6f7
井上大誠 <t.base.8901@icloud.com>
2021-12-09 16:25:46 +0900

    Add New method.

--- src/app/domain/model/room.go
+++ src/app/domain/model/room.go
@@ -1,12 +1,33 @@
 package model
 
+import (
+	"fmt"
+)
+
 type Room struct {
-	ID               string //uuid
 	Name			 string
 	StartSignForward chan struct{}
-	players          []*string //PlayerのID
+	players          []*Player //PlayerのID
 	questions		 []string
 
 	// CmdForward       chan shell.ExecResult
 	// ScoreForward     chan score.ScoreResult
+}
+
+func NewRoom(name string) *Room {
+	return &Room {
+		Name: name,
+	}
+}
+
+func (r *Room) GetPlayers() []*Player {
+	return r.players
+}
+
+func (r *Room) Accept(player *Player) error {
+	if len(r.GetPlayers()) > 2 {
+		return fmt.Errorf("This room is full.")
+	}
+	r.players = append(r.players, player)
+	return nil
 }
