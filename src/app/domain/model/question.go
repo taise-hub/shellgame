78900ac89d10a975f1c261c339a3e314948bbcb9
井上大誠 <t.base.8901@icloud.com>
2021-12-12 15:01:28 +0900

    Change Question property

--- src/app/domain/model/question.go
+++ src/app/domain/model/question.go
@@ -3,17 +3,13 @@ package model
 type Question struct {
 	ID     uint
 	Name   string
-	Difficulty string
 	Answer string
-	Score  int
 }
 
-func NewQuestion(id uint, name string, difficulty string, answer string, score int) *Question {
+func NewQuestion(id uint, name string, answer string) *Question {
 	return &Question {
 		ID: id,
 		Name: name,
-		Difficulty: difficulty,
 		Answer: answer,
-		Score: score,
 	}
 }
